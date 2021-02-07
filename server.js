dedec6b7670d69722199bb8e8ebc0282f1fd5ce9
Simone Westphal <hello@swestphal.io>
2021-02-07 16:28:37 +0100

    feature: Add handling of unhandled mongo error

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv')
-const logger = require('./middleware/logger')
+//const logger = require('./middleware/logger')
 const morgan = require('morgan');
+const connectDB = require('./config/db')
 
+// load env vars
+dotenv.config({ path: './config/config.env' })
+
+// connect to datatbase
+connectDB()
 
 const bootcamps = require('./routes/bootcamps')
 
 
-// load env vars
-dotenv.config({ path: './config/config.env' })
+
 
 const app = express();
 
@@ -27,4 +32,10 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => { console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`) })
+const server = app.listen(PORT, () => { console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`) })
+
+// handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1))
+})
