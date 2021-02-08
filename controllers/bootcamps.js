a11846e3442056dc0fceda630553727530902f25
Simone Westphal <hello@swestphal.io>
2021-02-08 12:32:09 +0100

    feature: Add custom errorhandler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,5 +1,5 @@
 const Bootcamp = require('./../models/Bootcamp');
-
+const ErrorResponse = require('../utils/errorResponse')
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -28,14 +28,14 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id)
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false })
+            return next(new ErrorResponse(`Bootcamp with id: ${req.params.id} not found.`, 404))
         }
         res.status(200).json({
             success: true,
             data: bootcamp
         })
     } catch (err) {
-        res.status(400).json({ success: false })
+        next(new ErrorResponse(`Bootcamp with id: ${req.params.id} not found.`, 404))
     }
 }
 
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 //const logger = require('./middleware/logger')
 const morgan = require('morgan');
 const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
 
 // load env vars
 dotenv.config({ path: './config/config.env' })
@@ -32,6 +33,7 @@ if (process.env.NODE_ENV === "development") {
 // mount routes
 app.use('/api/v1/bootcamps', bootcamps)
 
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
